c8557bec16aaf6b5405b55316a246630c70bfe4e
Tomer Figenblat <tfigenbl@redhat.com>
2025-09-19 08:21:52 -0400

    added toolhive operator component initial code
    Signed-off-by: Tomer Figenblat <tfigenbl@redhat.com>

---
+++ internal/controller/components/toolhiveoperator/toolhiveoperator_controller_actions.go
@@ -0,0 +1,47 @@
+package toolhiveoperator
+
+import (
+	"context"
+	"fmt"
+
+	componentApi "github.com/opendatahub-io/opendatahub-operator/v2/api/components/v1alpha1"
+	odhtypes "github.com/opendatahub-io/opendatahub-operator/v2/pkg/controller/types"
+	odhdeploy "github.com/opendatahub-io/opendatahub-operator/v2/pkg/deploy"
+)
+
+func initialize(_ context.Context, rr *odhtypes.ReconciliationRequest) error {
+	rr.Manifests = append(rr.Manifests, manifestsPath())
+	return nil
+}
+
+func loadParams(ctx context.Context, rr *odhtypes.ReconciliationRequest) error {
+	if err := odhdeploy.ApplyParams(paramsPath, "params.env", nil, map[string]string{"namespace": rr.DSCI.Spec.ApplicationsNamespace}); err != nil {
+		return fmt.Errorf("failed to update params.env from %s : %w", paramsPath, err)
+	}
+	return nil
+}
+
+func devFlags(ctx context.Context, rr *odhtypes.ReconciliationRequest) error {
+	toolhiveoperator, ok := rr.Instance.(*componentApi.ToolHiveOperator)
+	if !ok {
+		return fmt.Errorf("resource instance %v is not a componentApi.ToolHiveOperator)", rr.Instance)
+	}
+
+	if toolhiveoperator.Spec.DevFlags == nil {
+		return nil
+	}
+
+	// If dev flags are set, update default manifests path
+	if len(toolhiveoperator.Spec.DevFlags.Manifests) != 0 {
+		manifestConfig := toolhiveoperator.Spec.DevFlags.Manifests[0]
+		if err := odhdeploy.DownloadManifests(ctx, ComponentName, manifestConfig); err != nil {
+			return err
+		}
+		if manifestConfig.SourcePath != "" {
+			rr.Manifests[0].Path = odhdeploy.DefaultManifestPath
+			rr.Manifests[0].ContextDir = ComponentName
+			rr.Manifests[0].SourcePath = manifestConfig.SourcePath
+		}
+	}
+	return nil
+}
